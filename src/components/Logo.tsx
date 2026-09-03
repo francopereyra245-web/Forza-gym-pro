33e15be99cdc887ea2e1d9ed214c6cbf26d8ad16
francopereyra245-web <franco.pereyra245@gmail.com>
2026-09-03 07:37:42 -0300

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,9 @@
 export default function Logo() {
   return (
     <img
-      src="/logo.png"
+      src="/logo.png?v=2"
       alt="Forza Gym Pro"
-      className="h-12 w-auto object-contain"
+      className="h-12 w-auto object-contain rounded-md"
     />
   );
 }
